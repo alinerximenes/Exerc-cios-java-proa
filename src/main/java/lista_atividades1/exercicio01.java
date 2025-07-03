b2957354221f017dafcee0866cc92d80d723d02c
Aline Ximenes <alineproa123@gmail.com>
2025-07-02 21:17:17 -0300

    Add files via upload

---
+++ src/main/java/lista_atividades1/exercicio01.java
@@ -0,0 +1,24 @@
+package lista_atividades1;
+
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite sua idade em anos: ");
+        int anos = entrada.nextInt();
+
+        System.out.print("Digite os meses: ");
+        int meses = entrada.nextInt();
+
+        System.out.print("Digite os dias: ");
+        int dias = entrada.nextInt();
+
+        int totalDias = (anos * 365) + (meses * 30) + dias;
+
+
+        System.out.println(anos + " anos, " + meses + " meses e " + dias);
+        System.out.println("Você viveu aproximadamente " + totalDias + " dias.");
+    }
+}
---
+++ src/main/java/lista_atividades1/exercicio03.java
@@ -0,0 +1,20 @@
+package lista_atividades1;
+
+import java.util.Scanner;
+
+public class exercicio03 {
+
+        public static void main(String[] args) {
+
+            Scanner entrada = new Scanner(System.in);
+
+            System.out.println("Informe o saldo: ");
+
+            float saldo = entrada.nextFloat();
+
+            double reajuste = saldo * 1.01;
+
+            System.out.println("Saldo com reajuste de 1%: R$ " + reajuste);
+
+    }
+}
